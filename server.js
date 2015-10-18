6755a531bec626034f406cbd661ab799ad77a6e4
Ben Rogers <ben@bdr.org>
2015-10-18 17:46:52 +0000

    Initial website

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var swig = require('swig');
+
+var app = express();
+
+app.engine('html', swig.renderFile);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+app.set('view cache', false);
+swig.setDefaults({ cache: false });
+app.use(express.static('static'));
+
+app.get('/', function (req, res) {
+  res.render('index');
+});
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
